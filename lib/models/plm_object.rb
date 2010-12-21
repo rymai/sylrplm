013d0ab8524a324d66c4873c70b1596ffc32a1bd
Rémy Coutable <rymai@rymai.com>
2010-12-21 18:14:50 +0100

    Fixed indentation

--- lib/models/plm_object.rb
+++ lib/models/plm_object.rb
@@ -1,11 +1,10 @@
-#require 'fileutils'
-module Models::PlmObject 
-  #include FileUtils
+module Models::PlmObject
+
   # modifie les attributs avant edition
   def self.included(base)
     base.extend(ClassMethods) # ça appelle extend du sous module ClassMethods sur "base", la classe dans laquelle tu as inclue la lib
   end
-  
+
   module ClassMethods
     # inutilisee, voir application_controller.get_models_and_columns
     def self.get_columns
@@ -18,76 +17,72 @@ module Models::PlmObject
       ret
     end
   end
-    
-    def edit()
-      self.date=DateTime::now()
-    end
-    
-    def is_freeze
-      if(self.statusobject!=nil && Statusobject.find_last(self.class.name)!=nil)
-        if(self.statusobject.rank == Statusobject.find_last(self.class.name).rank)
-          true
-        else
-          false
-        end
-      else
-        false
-      end
-    end
-    
-    # a valider si avant dernier status
-    def is_to_validate
-      if(self.statusobject!=nil && Statusobject.find_last(self.class.name)!=nil)
-        if(self.statusobject.rank == Statusobject.find_last(self.class.name).rank-1)
-          true
-        else
-          false
-        end
-      else
-        false
-      end 
-    end
-    
-    def is_checked
-      check=Check.findCheckout(self.class.name, self) 
-      file=self.filename
-      if(check.nil?)
-        #non reserve
-        false
-      else
-        #reserve
+
+  def edit()
+    self.date=DateTime::now()
+  end
+
+  def is_freeze
+    if(self.statusobject!=nil && Statusobject.find_last(self.class.name)!=nil)
+      if(self.statusobject.rank == Statusobject.find_last(self.class.name).rank)
         true
+      else
+        false
       end
+    else
+      false
     end
-    
-    
-    
-    def revise 
-      if(self.is_freeze)
-        # recherche si c'est la derniere revision
-        rev_cur=self.revision
-        last_rev=find_last_revision(self)
-        puts "plm_object.revise:"+rev_cur.to_s+"->"+last_rev.revision.to_s
-        if(last_rev.revision==rev_cur)
-          obj=clone()      
-          obj.revision=rev_cur.next
-          obj.statusobject=Statusobject.find_first(self.class.name)
-          puts self.class.name+".revise:"+rev_cur+"->"+obj.revision
-          if self.has_attribute?(:filename)
-            if(self.filename!=nil)
-              content=self.read_file
-              obj.write_file(content)
-            end
+  end
+
+  # a valider si avant dernier status
+  def is_to_validate
+    if(self.statusobject!=nil && Statusobject.find_last(self.class.name)!=nil)
+      if(self.statusobject.rank == Statusobject.find_last(self.class.name).rank-1)
+        true
+      else
+        false
+      end
+    else
+      false
+    end
+  end
+
+  def is_checked
+    check=Check.findCheckout(self.class.name, self)
+    file=self.filename
+    if(check.nil?)
+      #non reserve
+      false
+    else
+      #reserve
+      true
+    end
+  end
+
+  def revise
+    if(self.is_freeze)
+      # recherche si c'est la derniere revision
+      rev_cur=self.revision
+      last_rev=find_last_revision(self)
+      puts "plm_object.revise:"+rev_cur.to_s+"->"+last_rev.revision.to_s
+      if(last_rev.revision==rev_cur)
+        obj=clone()
+        obj.revision=rev_cur.next
+        obj.statusobject=Statusobject.find_first(self.class.name)
+        puts self.class.name+".revise:"+rev_cur+"->"+obj.revision
+        if self.has_attribute?(:filename)
+          if(self.filename!=nil)
+            content=self.read_file
+            obj.write_file(content)
           end
-          return obj
-        else
-          return nil
         end
+        return obj
       else
         return nil
-      end    
+      end
+    else
+      return nil
     end
-    
-    
-  
+  end
+
 end
