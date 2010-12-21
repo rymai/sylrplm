bd225dac5f0f2ce88581faf9e8b97012d8249276
Rémy Coutable <rymai@rymai.com>
2010-12-21 18:12:25 +0100

    Name well the modules in /lib

--- lib/classes/app_classes.rb
+++ lib/classes/app_classes.rb
@@ -1,11 +1,12 @@
-module AppClasses
+module Classes::AppClasses
+
   # Build a Logger::Formatter subclass.
   class LogFormatter < Logger::Formatter
     # Provide a call() method that returns the formatted message.
     def call(severity, time, program_name, message)
       datetime      = time.strftime("%Y-%m-%d %H:%M")
       if severity == "ERROR"
-        
+
         print_message = "!!! #{String(message)} (#{datetime}) !!!"
         border        = "!" * print_message.length
         [border, print_message, border].join("\n") + "\n"
@@ -15,5 +16,5 @@ module AppClasses
       end
     end
   end
-end
 
+end
