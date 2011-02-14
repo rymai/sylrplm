a47b60aebc28f5eefa5d09b33b52993c1f54c770
sylvani <sylvani@laposte.net>
2011-02-14 22:39:01 +0100

    index menu toolbar at the top

--- app/models/access.rb
+++ app/models/access.rb
@@ -1,10 +1,9 @@
 #require 'lib/models/sylrplm_common'
 class Access < ActiveRecord::Base
   include Models::SylrplmCommon
-  
+
   validates_presence_of :controller, :roles , :action
   #validates_uniqueness_of :controller ,:role_id,:action
-  
   def self.create_new(i_controller, i_roles)
     obj=Access.new
     conts=i_controller.split(".")
@@ -14,41 +13,41 @@ class Access < ActiveRecord::Base
     obj.set_default_values(true)
     obj
   end
-  
+
   def self.find_for_controller(i_controller)
     ret={}
     #puts 'access.find_for_controller='+i_controller
     self.find(:all, :order=>"controller",
-          :conditions => ["controller like '#{i_controller}'"]).each do |acc|
+    :conditions => ["controller like '#{i_controller}'"]).each do |acc|
       ret[acc.action.to_sym]=acc.roles
     end
     ret
   end
-  
+
   def self.init
     ret=true
     Controller.get_controllers.each do |controller|
       st=0
       if controller.name !="AccessesController" &&
-        controller.name !="LoginController" &&
-        controller.name !="PlmInitController" &&
-        controller.name !="RolesController" &&
-        controller.name !="RolesUsersController" &&
-        controller.name !="SequencesController" &&
-        controller.name !="SessionsController"
-        st=st+create_access(controller.name,controller.method,"admin & !designer & !consultant") 
+      controller.name !="LoginController" &&
+      controller.name !="PlmInitController" &&
+      controller.name !="RolesController" &&
+      controller.name !="RolesUsersController" &&
+      controller.name !="SequencesController" &&
+      controller.name !="SessionsController"
+        st=st+create_access(controller.name,controller.method,"admin & !designer & !consultant")
       else
         if controller.name =="SessionsController"
-          st=st+create_access(controller.name,controller.method,"admin | designer | consultant") 
+          st=st+create_access(controller.name,controller.method,"admin | designer | consultant")
         else
-          st=st+create_access(controller.name,controller.method,"(admin | designer) & !consultant") 
+          st=st+create_access(controller.name,controller.method,"(admin | designer) & !consultant")
         end
       end
-      #puts 'access_controller.index:controller='+controller.name+' method='+controller.method+' st='+st.to_s     
+      #puts 'access_controller.index:controller='+controller.name+' method='+controller.method+' st='+st.to_s
     end
     true
   end
-  
+
   def self.create_access(ctrl,met,roles)
     access=new
     access.controller=ctrl
@@ -56,17 +55,17 @@ class Access < ActiveRecord::Base
     access.roles=roles
     puts "access.create_access:"+access.inspect
     if !access.save
-      return 1 
+      return 1
     else
       return 0
     end
   end
-  
+
   def self.get_conditions(filter)
     filter=filter.gsub("*","%")
     ["controller LIKE ? or action LIKE ? or roles LIKE ? " ,
-    filter, filter, 
-    filter] unless filter.nil? 
+      filter, filter,
+      filter] unless filter.nil?
   end
-  
+
 end
