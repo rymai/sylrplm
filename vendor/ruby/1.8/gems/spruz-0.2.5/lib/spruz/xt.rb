8a1d7b2e9739c989247d97a03a1d1b096835ff83
sylvani <sylvani@laposte.net>
2011-07-14 22:46:07 +0200

    integrating ruote

---
+++ vendor/ruby/1.8/gems/spruz-0.2.5/lib/spruz/xt.rb
@@ -0,0 +1,19 @@
+require 'spruz'
+
+module Spruz
+  require 'spruz/xt/blank'
+  require 'spruz/xt/count_by'
+  require 'spruz/xt/full'
+  require 'spruz/xt/hash_union'
+  require 'spruz/xt/irb'
+  require 'spruz/xt/null'
+  require 'spruz/xt/round'
+  require 'spruz/xt/subhash'
+  require 'spruz/xt/time_dummy'
+  require 'spruz/xt/uniq_by'
+  require 'spruz/xt/p'
+  require 'spruz/xt/symbol_to_proc'
+  require 'spruz/xt/write'
+  require 'spruz/xt/deep_dup'
+  require 'spruz/xt/attempt'
+end
