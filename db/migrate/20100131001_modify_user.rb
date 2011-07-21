360b008520894bd11a55f3e0c741007a19905506
sylvani <sylvani@laposte.net>
2011-07-21 22:53:20 +0200

    heroku migrate

--- db/migrate/20100131001_modify_user.rb
+++ db/migrate/20100131001_modify_user.rb
@@ -1,11 +1,9 @@
 class ModifyUser < ActiveRecord::Migration
   def self.up
-    rename_column :users, :name, :login
-    rename_column :users, :mail, :email
-    rename_column :roles, :name, :title
+    #rename_column :users, :name, :login
+    #rename_column :users, :mail, :email
+    #rename_column :roles, :name, :title
      
-        
-    
   end
 
   def self.down
