d03b79bc5386ae34dd49e579cdc15edcceacdc6c
sylvani <sylvani@laposte.net>
2016-09-27 21:34:46 +0200

    version ruby pour heroku

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
-class ApplicationMailer < ActiveRecord::Observer
+class ApplicationMailer < ActionMailer::Base
   default from: "sylvere.coutable@laposte.net"
   layout 'mailer'
   #content_type "text/html"
