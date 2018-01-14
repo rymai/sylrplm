0c8491281fc58a6a7a6926ee98b1273d219177d1
Rémy Coutable <remy@rymai.me>
2018-01-14 19:14:24 +0100

    Fix log level for staging
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- config/initializers/sylrplm_init.rb
+++ config/initializers/sylrplm_init.rb
@@ -67,14 +67,17 @@ NOLOGS = [
 LOG.formatter = Classes::AppClasses::LogFormatter.new(NOLOGS)
 # @logger.datetime_format = "%Y-%m-%d %H:%M:%S"
 # DEBUG INFO WARN ERROR FATAL
-LOG.level = case Rails.env
-            when 'development'
-              Logger::DEBUG
-            when 'test'
-              Logger::DEBUG
-            when 'production'
-              Logger::DEBUG
-end
+LOG.level =
+  case Rails.env
+  when 'development'
+    Logger::DEBUG
+  when 'test', 'staging'
+    Logger::WARN
+  when 'production'
+    Logger::ERROR
+  else
+    Logger::FATAL
+  end
 LOG.info('sylrplm') { 'Lancement SYLRPLM' }
 LOG.progname = 'Constantes'
 LOG.info { 'Constantes du module SYLRPLM:' }
